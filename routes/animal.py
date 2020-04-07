ed0bd397283090a950631ab1e644e15c5f086283
Mladen Popadic <mpopadic@griddynamics.com>
2020-04-07 18:19:42 +0200

    Check if Species exist when creating Animal

--- routes/animal.py
+++ routes/animal.py
@@ -2,7 +2,7 @@ import json
 from flask import current_app as app
 from flask import jsonify, request, Response
 
-from models import Animal
+from models import Animal, Species
 
 
 @app.route('/animals', methods=['GET'])
@@ -19,13 +19,26 @@ def get_animal_by_id(id):
 def add_animal():
     request_data = request.get_json()
     if Animal.is_valid_object(request_data):
-        Animal.add_animal(request_data['name'], request_data['center_id'], request_data['age'], request_data['species'], request_data['price'], request_data['description'])
-        response = Response('', status=201, mimetype='application/json')
-        return response
+        species = Species.query.filter_by(id=request_data['species']).first()
+        if species:
+            Animal.add_animal(request_data['name'],
+                              request_data['center_id'],
+                              request_data['age'],
+                              species.id,
+                              request_data['price'],
+                              request_data['description'])
+            response = Response('', status=201, mimetype='application/json')
+            return response
+        else:
+            error_object = {
+                "error": "Species with id {} doesn't exist. You must create species first.".format(request_data['species'])
+            }
+            return Response(json.dumps(error_object), 400)
+
     else:
         invalid_obj = {
             'error': "Valid Animal must contain: name, center_id, age and species"
         }
-        response = Response(json.dumps(invalid_obj), 400)
-        return response
+        return Response(json.dumps(invalid_obj), 400)
+
 
